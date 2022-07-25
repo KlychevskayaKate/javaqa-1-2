6bcbd2b964fdc1c4c96f7e5fb9c1f34bb82ed00d
Klychevskaya Kate <kateklych@gmail.com>
2022-07-25 22:24:18 +0300

    add file

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticket = 1000; // стоимость билета
+        int money = 20; // рублей
+
+        int miles = ticket / money;
+
+        System.out.println("Начисленные мили:" + miles);
+
+    }
+}
